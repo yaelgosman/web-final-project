b9191117360fdbe1014b3793ac90bbcf38992449
VegetaSaiyanZ <ofir6721@gmail.com>
2026-01-24 00:03:31 +0200

    Added Google Registration + error msgs

--- client/src/constants/errors.ts
+++ client/src/constants/errors.ts
@@ -2,3 +2,10 @@ export const INVALID_LOGIN_ERRORS = {
     INVALID_CREDENTIALS: 'אחד או יותר מפרטי ההתחברות לא נכונים!',
     EMPTY_CREDENTIALS: 'יש למלא את כל השדות',
 };
+
+export const INVALID_REGISTRATION_ERRORS = {
+    MISSING_FIELDS: 'יש למלא את כל השדות',
+    USERNAME_TAKEN: 'שם המשתמש הזה כבר תפוס',
+    DIFFERENT_PASSWORDS: 'יש לוודא כי הסיסמאות זהות',
+    
+}
