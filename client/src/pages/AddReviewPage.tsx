0f83f926b67ed73d3dc8b24c88e9412cfb142092
VegetaSaiyanZ <ofir6721@gmail.com>
2026-03-17 13:13:40 +0200

    Added navigation to home page on successful post creation

--- client/src/pages/AddReviewPage.tsx
+++ client/src/pages/AddReviewPage.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate, Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import AddReview from '../components/AddReview/AddReview';
 
 const AddReviewPage: React.FC = () => {
   const { user, isAuthenticated } = useAuth();
+  const navigate = useNavigate(); 
 
   if (!isAuthenticated || !user || !user._id) {
     return <Navigate to="/Login" replace />;
   }
 
+  // TODO: add an indication for the user for "Successful upload / error in upload"
   return (
     <AddReview 
       userId={user._id} 
-      onPostSuccess={() => console.log("Post successful!")} 
+      onPostSuccess={() => navigate("/")}
     />
   );
 };
